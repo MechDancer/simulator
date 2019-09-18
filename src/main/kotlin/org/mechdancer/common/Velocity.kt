5e4dffd7834534012c18e0eb3db5eb35c85e548d
YangDR <ydrml@hotmail.com>
2019-09-18 12:24:33 +0800

    统一全向底盘和非全向底盘运动学模型

--- src/main/kotlin/org/mechdancer/common/Velocity.kt
+++ src/main/kotlin/org/mechdancer/common/Velocity.kt
@@ -1,24 +1,53 @@
 package org.mechdancer.common
 
+import org.mechdancer.algebra.function.vector.times
+import org.mechdancer.algebra.implement.vector.vector2DOf
+import org.mechdancer.geometry.angle.rotate
+import org.mechdancer.geometry.angle.toAngle
+import org.mechdancer.geometry.angle.toRad
+import kotlin.math.abs
+import kotlin.math.cos
+import kotlin.math.sin
+
 /** 标准底盘运动学模型下的控制量 */
 sealed class Velocity {
+    abstract fun toDeltaOdometry(dt: Double): Odometry
+
     /** 静止不动 */
-    object Static : Velocity()
+    object Static : Velocity() {
+        override fun toDeltaOdometry(dt: Double) = Odometry()
+    }
 
     /** 非全向 */
     data class NonOmnidirectional(
         val v: Double,
         val w: Double
-    ) : Velocity()
+    ) : Velocity() {
+        override fun toDeltaOdometry(dt: Double) =
+            Omnidirectional(v, .0, w).toDeltaOdometry(dt)
+    }
 
     /** 全向 */
     data class Omnidirectional(
         val vx: Double,
         val vy: Double,
         val w: Double
-    ) : Velocity()
+    ) : Velocity() {
+        override fun toDeltaOdometry(dt: Double) =
+            if (abs(w) < DoubleEpsilon) {
+                Odometry(vector2DOf(vx * dt, vy * dt), .0.toRad())
+            } else {
+                val v = vector2DOf(vx, vy)
+                val r = v.length / w
+                val theta = w * dt
+                Odometry(vector2DOf(sin(theta), 1 - cos(theta)) * r rotate v.toAngle(),
+                         theta.toRad())
+            }
+    }
 
     companion object {
+        const val DoubleEpsilon = 5E-15
+
         fun velocity() = Static
 
         fun velocity(v: Number, w: Number) =
--- src/main/kotlin/org/mechdancer/simulation/Chassis.kt
+++ src/main/kotlin/org/mechdancer/simulation/Chassis.kt
@@ -1,19 +1,12 @@
 package org.mechdancer.simulation
 
-import org.mechdancer.algebra.function.vector.times
-import org.mechdancer.algebra.implement.vector.vector2DOf
-import org.mechdancer.common.Odometry
-import org.mechdancer.common.Stamped
+import org.mechdancer.common.*
 import org.mechdancer.common.Stamped.Companion.stamp
-import org.mechdancer.common.Velocity
-import org.mechdancer.common.Velocity.*
+import org.mechdancer.common.Velocity.Static
 import org.mechdancer.common.filters.Filter
-import org.mechdancer.geometry.angle.toRad
 import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.sin
 
-/** 初始状态为 [origin] 的通用底盘模型 */
+/** 通用底盘模型 */
 class Chassis(origin: Stamped<Odometry>? = null) : Filter<Velocity, Stamped<Odometry>> {
     /** 位姿 */
     var odometry = origin ?: stamp(Odometry())
@@ -23,35 +16,26 @@ class Chassis(origin: Stamped<Odometry>? = null) : Filter<Velocity, Stamped<Odom
     var velocity: Velocity = Static
         private set
 
+    /** 更新速度 */
+    fun drive(velocity: Velocity, time: Long? = null) = update(velocity, time)
+
     /** 计算当前位姿 */
     operator fun get(time: Long? = null): Odometry {
         val dt = ((time ?: System.currentTimeMillis()) - odometry.time) / 1000.0
         require(dt >= 0)
         return when {
-            abs(dt) < DoubleEpsilon -> odometry.data
+            abs(dt) < DoubleEpsilon ->
+                odometry.data
             dt > 0                  -> {
-                when (val x = velocity) {
-                    is Static             -> odometry.data
-                    is NonOmnidirectional -> {
-                        val (v, w) = x
-                        odometry.data plusDelta
-                            if (abs(w) < DoubleEpsilon) {
-                                Odometry(vector2DOf(v * dt, 0), .0.toRad())
-                            } else {
-                                val r = v / w
-                                val theta = w * dt
-                                Odometry(vector2DOf(sin(theta), 1 - cos(theta)) * r, theta.toRad())
-                            }
-                    }
-                    is Omnidirectional    -> TODO()
+                when (val copy = velocity) {
+                    is Static -> odometry.data
+                    else      -> (odometry.data.toTransformation() * copy.toDeltaOdometry(dt).toTransformation()).toPose()
                 }
             }
-            else                    -> throw IllegalArgumentException("cannot get previous pose")
+            else                    ->
+                throw IllegalArgumentException("cannot get previous pose")
         }
     }
-
-    /** 更新速度 */
-    fun drive(velocity: Velocity, time: Long? = null) = update(velocity, time)
 
     override fun update(new: Velocity, time: Long?): Stamped<Odometry> {
         val now = time ?: System.currentTimeMillis()
