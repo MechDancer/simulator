397e7f911164ebff80b6257ec629728d2284b12f
YangDR <ydrml@hotmail.com>
2019-09-27 21:11:51 +0800

    测试

--- src/test/kotlin/org/mechdancer/simulation/Main.kt
+++ src/test/kotlin/org/mechdancer/simulation/Main.kt
@@ -2,13 +2,14 @@ package org.mechdancer.simulation
 
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.channels.produce
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
+import org.mechdancer.common.Odometry
+import org.mechdancer.common.Odometry.Companion.odometry
 import org.mechdancer.common.filters.Differential
 import org.mechdancer.common.toPose
 import org.mechdancer.simulation.Default.newOmniRandomDriving
 import org.mechdancer.simulation.Default.remote
+import org.mechdancer.simulation.Default.speedSimulation
 import org.mechdancer.struct.StructBuilderDSL.Companion.struct
 import kotlin.math.PI
 import kotlin.math.pow
@@ -19,13 +20,13 @@ private val robot = struct(Chassis()) {
     "定位模块" asSub {
         pose(0.31, 0)
         Encoder(0) asSub {
-            pose(0, 0.05, PI)
+            pose(0, +.2, 0)
         }
         Encoder(1) asSub {
-            pose(0.05, 0, PI / 2)
+            pose(0, -.2, PI / 2)
         }
         Encoder(2) asSub {
-            pose(0, -0.05, PI / 4)
+            pose(0, 0, PI / 4)
         }
     }
 }
@@ -40,17 +41,15 @@ private val encodersOnRobot =
 fun main() = runBlocking {
     // 离散差分环节
     val differential = Differential(robot.what.get()) { _, old, new -> new minusState old }
-    produce {
-        newOmniRandomDriving().run {
-            while (true) {
-                send(next())
-                delay(100L)
-            }
-        }
-    }.consumeEach { v ->
+    val dValue = List(3) { Differential(.0) { _, old, new -> new - old } }
+    val random = newOmniRandomDriving()
+    var pose = Odometry()
+    speedSimulation(this) {
+        random.next()
+    }.consumeEach { (_, v) ->
         //  计算机器人位姿增量
-        robot.what.drive(v)
-            .data
+        val actual = robot.what.drive(v).data
+        actual
             .also { pose -> remote.paint("pose", pose.p.x, pose.p.y, pose.d.asRadian()) }
             .let { differential.update(it) }
             .data
@@ -60,23 +59,32 @@ fun main() = runBlocking {
                     .onEach { (encoder, pose) -> encoder.update(pose, delta) }
             }
             .keys
-            .also(::println)
+            .let { set ->
+                val v0 = dValue[0].update(set.single { it.key == 0 }.value).data
+                val v90 = dValue[1].update(set.single { it.key == 1 }.value).data
+                val v45 = dValue[2].update(set.single { it.key == 2 }.value).data
+                val dp = odometry(
+                    x(+.2, .0, v0, v90, v45),
+                    y(+.2, .0, v0, v90, v45),
+                    theta(+.2, .0, v0, v90, v45))
+                pose = pose plusDelta dp
+                println("actual = $actual, pose = $pose")
+            }
     }
 }
 
-
 fun x(e0y: Double, e90x: Double, v0: Double, v90: Double, v45: Double) =
     (2.0 * (2 * e90x.pow(2) + e0y * (-2 * e0y * (v90 - sqrt(2.0) * v45)) + v90 * (v0 + v90) - sqrt(2.0) * v45) +
-            e90x * (v0 * (v0 + v90) - sqrt(2.0) * (2 * v0 + v90) * v45 + 2 * v45.pow(2) - e0y * (v0 - v90 + sqrt(2.0) * v45))) /
-            (4 * (e90x - e0y).pow(2) + v0.pow(2) + v90.pow(2) - 2 * sqrt(2.0) * v90 * v45 + 2 * v45.pow(2))
+     e90x * (v0 * (v0 + v90) - sqrt(2.0) * (2 * v0 + v90) * v45 + 2 * v45.pow(2) - e0y * (v0 - v90 + sqrt(2.0) * v45))) /
+    (4 * (e90x - e0y).pow(2) + v0.pow(2) + v90.pow(2) - 2 * sqrt(2.0) * v90 * v45 + 2 * v45.pow(2))
 
 fun y(e0y: Double, e90x: Double, v0: Double, v90: Double, v45: Double) =
     (2 * (e90x * (-2 * e0y + v0) * v90 - 2 * e90x.pow(2) * (v0 - sqrt(2.0) * v45) + e90x * (2 * e0y + v0) * (v0 - sqrt(
         2.0
     ) * v45) +
-            e0y * (v90 * (2 * e0y + v0 + v90) - sqrt(2.0) * (v0 + 2 * v90) * v45 + 2 * v45.pow(2)))) /
-            (4 * (e90x - e0y).pow(2) + v0.pow(2) + v90.pow(2) - 2 * sqrt(2.0) * v90 * v45 + 2 * v45.pow(2) + 2 * v0 * (v90 - sqrt(
-                2.0
-            ) * v45))
+          e0y * (v90 * (2 * e0y + v0 + v90) - sqrt(2.0) * (v0 + 2 * v90) * v45 + 2 * v45.pow(2)))) /
+    (4 * (e90x - e0y).pow(2) + v0.pow(2) + v90.pow(2) - 2 * sqrt(2.0) * v90 * v45 + 2 * v45.pow(2) + 2 * v0 * (v90 - sqrt(
+        2.0
+    ) * v45))
 
 fun theta(e0y: Double, e90x: Double, v0: Double, v90: Double, v45: Double) = (v0 + v90 - sqrt(2.0) * v45) / (e90x - e0y)
