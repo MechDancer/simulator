01280a789d31add06124c71b9d05c30b4c8f1c55
YangDR <ydrml@hotmail.com>
2019-10-06 20:05:22 +0800

    升级 gradle，添加科创仿真示例(Main)

--- src/test/kotlin/org/mechdancer/simulation/Main.kt
+++ src/test/kotlin/org/mechdancer/simulation/Main.kt
@@ -3,17 +3,33 @@ package org.mechdancer.simulation
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.runBlocking
+import org.mechdancer.algebra.function.vector.component1
+import org.mechdancer.algebra.function.vector.component2
+import org.mechdancer.algebra.implement.vector.vector2DOf
+import org.mechdancer.common.Odometry
 import org.mechdancer.common.Stamped
+import org.mechdancer.common.Velocity.Companion.velocity
+import org.mechdancer.common.invoke
+import org.mechdancer.common.toTransformation
 import org.mechdancer.simulation.Default.newOmniRandomDriving
 
+private const val T0 = 0L
+private val targetOnPerson = vector2DOf(-.5, 0)
+
 @ExperimentalCoroutinesApi
-fun main() = runBlocking<Unit> {
+fun main() = runBlocking {
     val behavior = newOmniRandomDriving()
-//    val person = Chassis(Stamped())
-    val robot = Chassis()
-    speedSimulation {
+    val person = Chassis(Stamped(T0, Odometry()))
+    val robot = Chassis(Stamped(T0, Odometry()))
+    speedSimulation(speed = -50) {
         behavior.next()
-    }.consumeEach {
-        println(it)
+    }.consumeEach { (t, v) ->
+        val (_, personOnMap) = person.drive(v, t)
+        val mapToRobot = -robot[t].toTransformation()
+        val personOnRobot = mapToRobot(personOnMap)
+        val personToRobot = personOnRobot.toTransformation()
+        val (x, y) = personToRobot(targetOnPerson)
+        robot.drive(velocity(.01 * x, .01 * y, .01 * personOnRobot.d.asRadian()), t)
+        println("$x, $y, ${personOnRobot.d}")
     }
 }
